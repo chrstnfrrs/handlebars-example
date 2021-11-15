127d530eb5f9ddd758e587574d0e3561a278d3c1
Christian Farris <christianfarriscs@gmail.com>
2021-11-14 20:20:12 -0600

    feat: refactor directory structure

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const pfs = require('promise-fs')
 const createTemplate = (template, data) => template(data)
 
 const main = async () => {
-  const templateFile = await pfs.readFile('./example.hbs')
+  const templateFile = await pfs.readFile('./src/templates/example.hbs')
   const templateString = await templateFile.toString()
   const template = await hb.compile(templateString)
 
@@ -14,7 +14,7 @@ const main = async () => {
 
   const filledTemplate = createTemplate(template, data)
   
-  await pfs.writeFile('./example.html', filledTemplate)
+  await pfs.writeFile('./src/generated/example.html', filledTemplate)
 }
 
 main()
